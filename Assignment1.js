282a858a7f6ffe622449564587080bdb646b8e37
sachin kumar <merugune.sachin123@gmail.com>
2021-07-06 19:39:04 +0530

    problems are done on Assignment operators

---
+++ Assignment1.js
@@ -0,0 +1,10 @@
+//Q1  with x=0 which of following statements will produce a value of 0?
+
+var x = 0;
+console.log(x++)
+
+var x2 = 0;
+console.log(x2=x2+1)
+
+var x3 = 0;
+console.log(x3+=1)
---
+++ Assignment2.js
@@ -0,0 +1,25 @@
+//Q2  with x=1 which of the following statements will produce a value of 0? 
+
+var x = 1
+console.log(x--)
+
+var x2 = 1
+console.log(x2=x2-1)
+
+var x3 = 1
+console.log(x3-=1)
+
+//Q3  what wii be result of num+=5*10/2
+
+var num = 100;
+console.log(num+=5*10/2)
+
+//Q4  what wii be the result?
+
+var num2 = 100;
+console.log(num2*=2*10/5)
+
+//Q5  what wii be the result?
+
+var num3 = 100;
+console.log(num3/=2*8-15)
